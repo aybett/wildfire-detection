3b1e728cc14cc986840deaa6765109c59e48d41b
aybett <aysebetulgunes@gmail.com>
2025-12-18 20:09:45 +0300

    Update dataset_test.py

--- dataset_test.py
+++ dataset_test.py
@@ -1,5 +1,6 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import os
 
 # Dataset yolu
 DATASET_PATH = "archive/the_wildfire_dataset_2n_version"
@@ -10,16 +11,24 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
+# Train, Validation ve Test datasetleri
 train_dataset = datasets.ImageFolder(
-    root=f"{DATASET_PATH}/train",
+    root=os.path.join(DATASET_PATH, "train"),
     transform=transform
 )
 
 val_dataset = datasets.ImageFolder(
-    root=f"{DATASET_PATH}/val",
+    root=os.path.join(DATASET_PATH, "val"),
     transform=transform
 )
 
+test_dataset = datasets.ImageFolder(
+    root=os.path.join(DATASET_PATH, "test"),
+    transform=transform
+)
+
+# Bilgileri yazdır
 print("Sınıflar:", train_dataset.classes)
 print("Eğitim veri sayısı:", len(train_dataset))
 print("Validation veri sayısı:", len(val_dataset))
+print("Test veri sayısı:", len(test_dataset))
